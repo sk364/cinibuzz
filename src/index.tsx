0a01a2d2bfa14c40b15e6037b4e06a034f4d193a
Sachin <skad5455@gmail.com>
2022-01-23 16:11:29 +0530

    Rename to scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,39 @@
+import './index.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import createMiddleware from "redux-saga";
+
+import PageLayout from "./components/PageLayout";
+import MovieList from "./screens/MovieList";
+import MovieDetail from "./screens/MovieDetail";
+
+import configureStore from "./store";
+import reducer from "./reducers";
+import sagas from "./sagas";
+
+const sagaMiddleware = createMiddleware();
+const store = configureStore(reducer, sagaMiddleware);
+
+sagaMiddleware.run(sagas);
+
+const NoMatch = () => {
+  return <div>Nothing here</div>;
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <PageLayout>
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route path="/:id" element={<MovieDetail />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </PageLayout>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
